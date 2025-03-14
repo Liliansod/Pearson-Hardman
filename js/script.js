8c8ad509de32f097b7ecf9200952557f63f35bf2
Lilian <liliansod22@gmail.com>
2025-03-13 22:00:00 -0300

    Atualização script de JavaScript Slide banner

--- js/script.js
+++ js/script.js
@@ -1,3 +1,57 @@
+// Selecionar os elementos dos slides
+const slides = document.querySelectorAll(".banner"); // Selecionar todos os id: banner
+//
+const dots = document.querySelectorAll(".dot");
+// Variavel de controle
+let sllideIndex = 0;
+
+function showSlides(){
+    for(let i=0; i < slides.length; i++){
+        
+    }
+}
+
+//Inicialização
+showSlides();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // DOM
 // Navegar no documento HTML
-const brand = document.querySelector("#brand"); // querySelector acessa e seleciona o HTML (Estou armazenado o conteudo do HTML dentro da variável brand criada no JavaScript)
+// const brand = document.querySelector("#brand"); // querySelector acessa e seleciona o HTML (Estou armazenado o conteudo do HTML dentro da variável brand criada no JavaScript)
+
+// brand.addEventListener('click', function(event){
+//     // console.log("testando o click");
+//     // console.log(event.target);
+//     event.target.style.color = "blue"; //Troca de cor da palavra dentro do HTML passando pelo CSS mas que teve ação do JavaScript 
+
+// }); // Função que escuta algum evendo(Aguma ação ex: clicar na página) acontecer no elemento colocado na variável
